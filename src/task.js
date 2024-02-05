127c0b0bc8d28bae1eaa1ac7b17a80fcf3707c9b
astrosuka <yegresvoselok@gmail.com>
2024-02-05 09:23:27 -0300

    Add getter & setter for priority

--- src/task.js
+++ src/task.js
@@ -8,5 +8,15 @@ export default class Task {
     }
     toggleComplete() {
         this.isComplete = this.isComplete ? false : true;
+    } 
+    set priority(newPriority) {
+        if (newPriority >= 1 && newPriority <= 3){
+            this._priority = newPriority;
+        } else {
+            console.error('Invalid Value!');
+        }
+    }
+    get priority() {
+        return this._priority;
     }
 }
