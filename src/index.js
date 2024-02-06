91f232c7c46ebf74da68e1c38c8279d1f9e60edd
astrosuka <yegresvoselok@gmail.com>
2024-02-06 09:12:15 -0300

    Add functions to create and delete tasks and lists

--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
 import Task from './task.js'
 import List from './list.js'
+import renderList from './render-list.js';
+import './style.css';
 
+const allLists = [];
 const defaultList = new List('My Tasks');
-console.log(defaultList.title);
+allLists.push(defaultList);
+let currentList;
+currentList = defaultList;
+
+function createList(name) {
+    allLists.push(new List(name));
+}
+
+const navMenu = document.querySelector('#nav-menu');
+let createListButton = document.createElement('button');
+createListButton.textContent = 'add new list'
+navMenu.appendChild(createListButton);
+createListButton.addEventListener('click', () => {
+    createList(prompt('Name your new list', 'New List' ))
+    displayAllLists();
+});
+
+displayAllLists();
+
+function displayAllLists() {
+    const listMenu = document.querySelector('#list-menu');
+    listMenu.textContent = '';
+    for (let i of allLists) {
+        const listName = document.createElement('li');
+        listName.textContent = i.title;
+        listMenu.append(listName);
+        listName.addEventListener('click', () => {
+            currentList = i;
+            renderList(currentList);
+            
+        })
+        const removeListButton = document.createElement('button');
+        removeListButton.textContent = 'x'
+        listMenu.append(removeListButton);
+        removeListButton.addEventListener('click', () => {
+            removeList(allLists.indexOf(i));
+            displayAllLists();
+        })
+    }
+
+    function removeList(index) {
+        console.log(allLists)
+        allLists.splice(index, 1);
+        //update DOM. HACER UNA FUNCION QUE REDIBUJE TODO ASI TAL COMO ESTA LA COSA
+    }
+}
+
+
+document.body.addEventListener('click', () => {
+    console.log(allLists);
+})
+
+renderList(currentList);
+
+export { currentList };
--- src/render-list.js
+++ src/render-list.js
@@ -1,4 +1,9 @@
 import renderCard from './render-card.js'
+import Task from './task.js';
+import { currentList } from './index.js';
+
+newTaskForm();
+const dialog = document.querySelector('#add-task');
 
 export default function renderList(list) {
     const wrapper = document.querySelector('#wrapper');
@@ -6,8 +11,53 @@ export default function renderList(list) {
     wrapper.textContent = '';
     listTitle.textContent = list.title;
     wrapper.appendChild(listTitle);
-    console.log(list)
+    
     for (let item of list.list) {        
         wrapper.appendChild(renderCard(item, list));
+    }
+
+    const addItemButton = document.createElement('button');
+    addItemButton.textContent = 'Add New Task';
+    addItemButton.addEventListener('click', () => {
+        dialog.showModal();
+    })
+
+    wrapper.appendChild(addItemButton);
+}
+
+function newTaskForm() {
+    const title = document.querySelector('#title');
+    
+    let description = document.querySelector('#description');
+
+    let date = document.querySelector('#date');
+
+    let priority = document.querySelectorAll('[name="priority"]');
+    let selectedPriority;
+    
+    const submitButton = document.querySelector('#submit-button');
+    submitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        for (const radioButton of priority) {
+            if (radioButton.checked) {
+                selectedPriority = radioButton.value;
+                break;
+            }
+        }
+        submit();
+    })
+
+    const cancelButton = document.querySelector('#cancel-button');
+    cancelButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        dialog.close();
+        document.querySelector('form').reset();
+    })
+    
+   function submit() {
+        currentList.addItem(new Task(title.value, description.value, date.value, +selectedPriority))
+        renderList(currentList);
+        dialog.close();
+        document.querySelector('form').reset();
     }
 }
